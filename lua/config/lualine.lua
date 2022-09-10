7315cfe4fb3b318bb2785dcec73912fcc9a232a4
Renszo D'Giovanni <varsovia84@gmail.com>
2022-09-10 16:03:30 +0200

    Modified lualine options.

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -6,22 +6,24 @@ if not status_ok then
 end
 
 lualine.setup {
+    options = {
+    component_separators = '|',
+    section_separators = { left = '', right = '' },
+  },
   sections = {
-    lualine_a = { 'mode' },
-    lualine_b = { 'branch' },
-    lualine_c = { {
-      'filename',
-      file_status = true, -- displays file status (readonly status, modified status)
-      path = 0 -- 0 = just filename, 1 = relative path, 2 = absolute path
-    } },
+    lualine_a = {'fileformat'},
+    lualine_b = { 'windows' },
+    lualine_c = {'branch'},
     lualine_x = {
       { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
         hint = ' ' } },
-      'encoding',
-      'filetype'
+      -- 'encoding',
+      -- 'filetype'
     },
-    lualine_y = { 'progress' },
-    lualine_z = { 'location' }
+    lualine_y = { 'filetype', 'progress' },
+    lualine_z = {
+      { 'mode' },
+    },
   },
   inactive_sections = {
     lualine_a = {},
@@ -36,5 +38,39 @@ lualine.setup {
     lualine_z = {}
   },
   tabline = {},
-  extensions = { 'fugitive' }
+  extensions = {'fugitive'},
 }
+
+-- lualine.setup {
+--   sections = {
+--     lualine_a = { 'mode' },
+--     lualine_b = { 'branch' },
+--     lualine_c = { {
+--       'filename',
+--       file_status = true, -- displays file status (readonly status, modified status)
+--       path = 0 -- 0 = just filename, 1 = relative path, 2 = absolute path
+--     } },
+--     lualine_x = {
+--       { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
+--         hint = ' ' } },
+--       'encoding',
+--       'filetype'
+--     },
+--     lualine_y = { 'progress' },
+--     lualine_z = { 'location' }
+--   },
+--   inactive_sections = {
+--     lualine_a = {},
+--     lualine_b = {},
+--     lualine_c = { {
+--       'filename',
+--       file_status = true, -- displays file status (readonly status, modified status)
+--       path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+--     } },
+--     lualine_x = { 'location' },
+--     lualine_y = {},
+--     lualine_z = {}
+--   },
+--   tabline = {},
+--   extensions = { 'fugitive' }
+-- }
