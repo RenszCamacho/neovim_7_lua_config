f7af6a627fbdc39447d4af9125e1f657431f0500
Renszo D'Giovanni <varsovia84@gmail.com>
2022-09-10 18:25:02 +0200

    Modify set icons.

--- lua/config/devicons.lua
+++ lua/config/devicons.lua
@@ -3,5 +3,22 @@ local status_ok, devicons = pcall(require, "nvim-web-devicons")
 if not status_ok then
 	return
 end
-devicons.get_icons()
 
+-- devicons.set_icon {
+--   javascript = {
+--     icon = "",
+--     color = "#428850",
+--     cterm_color = "65",
+--     name = "Javascript"
+--   }
+-- }
+
+devicons.get_icons()
+devicons.set_icon {
+  js = {
+    icon = "",
+    color = "#fff200",
+    cterm_color = "65",
+    name = "Javascript"
+  },
+}
