fd953f2d1c4e294b05f957938737915e82327bb7
Renszo D'Giovanni <varsovia84@gmail.com>
2022-09-11 10:25:49 +0200

    Set colorscheme base16-material-palenight.

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -5,11 +5,12 @@
 
 
 -- local ok, _ = pcall(vim.cmd, 'colorscheme base16-dracula')
+local ok, _ = pcall(vim.cmd, 'colorscheme base16-material-palenight')
 -- local ok, _ = pcall(vim.cmd, 'colorscheme base16-gruvbox-dark-medium')
 -- local ok, _ = pcall(vim.cmd, 'colorscheme base16-monokai')
 -- local ok, _ = pcall(vim.cmd, 'colorscheme base16-nord')
 -- local ok, _ = pcall(vim.cmd, 'colorscheme base16-oceanicnext')
-local ok, _ = pcall(vim.cmd, 'colorscheme base16-onedark')
+-- local ok, _ = pcall(vim.cmd, 'colorscheme base16-onedark')
 -- local ok, _ = pcall(vim.cmd, 'colorscheme palenight')
 -- local ok, _ = pcall(vim.cmd, 'colorscheme base16-solarized-dark')
 -- local ok, _ = pcall(vim.cmd, 'colorscheme base16-solarized-light')
